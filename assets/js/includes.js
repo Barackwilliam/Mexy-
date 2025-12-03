e6a5614ca19b96d69436576d4417f9c244947a8d
barackwilliam <barackwilliam12@gmail.com>
2025-12-03 04:32:19 +0300

    done

--- assets/js/includes.js
+++
@@ -1,8 +0,0 @@
-// Load header
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("header.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("header-placeholder").innerHTML = data;
-        });
-});
